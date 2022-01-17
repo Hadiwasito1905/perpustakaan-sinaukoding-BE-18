9aad0edee55256fdc541d9dfc6825122c50af0d5
Hadiwasito1905 <noverryambo@gmail.com>
2022-01-18 00:30:53 +0700

    Make DAO: BookDAO, LoanDAO, & UserDAO

---
+++ src/main/java/sinaukodingBE18/perpustakaan/dao/BookDAO.java
@@ -0,0 +1,31 @@
+package sinaukodingBE18.perpustakaan.dao;
+
+import org.springframework.stereotype.Repository;
+import sinaukodingBE18.perpustakaan.entity.Book;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class BookDAO extends BaseDAO<Book> {
+
+    @Override
+    public List<Predicate> predicates(Book param, CriteriaBuilder builder, Root<Book> root, boolean isCount) {
+        List<Predicate> predicates = super.predicates(param, builder, root, isCount);
+
+        if (param != null) {
+            if (param.getTitle() != null) {
+                predicates.add(builder.like(root.get("title"), "%" + param.getTitle() + "%"));
+            }
+
+            if (param.getPublisher() != null) {
+                predicates.add(builder.like(root.get("publisher"), "%" + param.getPublisher() + "%"));
+            }
+
+        }
+
+        return predicates;
+    }
+}
---
+++ src/main/java/sinaukodingBE18/perpustakaan/dao/UserDAO.java
@@ -0,0 +1,27 @@
+package sinaukodingBE18.perpustakaan.dao;
+
+import org.springframework.stereotype.Repository;
+import sinaukodingBE18.perpustakaan.entity.User;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class UserDAO extends BaseDAO<User> {
+    @Override
+    public List<Predicate> predicates(User param, CriteriaBuilder builder, Root<User> root, boolean isCount) {
+        List<Predicate> predicates = super.predicates(param, builder, root, isCount);
+
+        if (param != null) {
+            if (param.getUsername() != null) {
+                predicates.add(builder.equal(root.get("username"), param.getUsername()));
+            }
+        }
+
+        return predicates;
+
+    }
+
+}
